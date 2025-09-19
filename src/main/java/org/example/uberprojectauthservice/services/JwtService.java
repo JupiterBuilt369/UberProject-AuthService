9c9b40d533c459bb22de9918814b8ae482abb86a
JupiterBuilt369 <ru.official.shivam@gmail.com>
2025-09-19 19:14:36 +0530

    Added UserDetailsServiceImpl

--- src/main/java/org/example/uberprojectauthservice/services/JwtService.java
+++ src/main/java/org/example/uberprojectauthservice/services/JwtService.java
@@ -1,5 +1,6 @@
 package org.example.uberprojectauthservice.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -7,11 +8,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService implements CommandLineRunner {
@@ -22,26 +24,66 @@ public class JwtService implements CommandLineRunner {
     @Value("${jwt.secret}")
     private String SECRET;
 
+    private Key getSignKey(){
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
 
-    private String generateToken(Map<String, Object> payload, String username) {
-        SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    private String generateToken(Map<String, Object> payload, String email) {
 
         return Jwts.builder()
                 .setClaims(payload)
-                .setSubject(username)
+                .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiry))
-                .signWith(key, SignatureAlgorithm.HS512)
+                .signWith(getSignKey(), SignatureAlgorithm.HS512)
                 .compact();
     };
+
+
+    private Claims extractAllPayloads(String token) {
+        return Jwts.parserBuilder()
+                            .setSigningKey(getSignKey())
+                            .build()
+                            .parseClaimsJws(token)
+                            .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllPayloads(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Object extractPayload(String token, String payloadKey){
+        Claims claim = (Claims) extractAllPayloads(token);
+        return (Object)claim.get(payloadKey);
+    }
+
+    private String extractEmail(String token) {
+        return extractClaim(token,Claims::getSubject);
+    }
+
+
+    public boolean isTokenValid(String token,  String email) {
+        final String userEmailFetchedFromToken = extractEmail(token);
+        return (userEmailFetchedFromToken.equals(email) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaim(token,Claims :: getExpiration).before(new Date());
+    }
 
     @Override
     public void run(String... args) throws Exception {
         Map<String,Object> payload = new HashMap<>();
-        payload.put("email","a@b.c" );
-        payload.put("username","a@b.c");
+        payload.put("email","a@b.c ");
+        payload.put("username","jaiShreeRam");
         payload.put("password","a123");
-        String jwtToken = generateToken(payload,"Shivam");
+        payload.put("phoneNumber","123456789");
+        String jwtToken = generateToken(payload,"a@b.c ");
+        System.out.println(isTokenValid(jwtToken,"a@b.c "));
         System.out.println("JWT Token: " + jwtToken);
+        System.out.println(extractPayload(jwtToken,"phoneNumber"));
+        System.out.println(extractPayload(jwtToken,"email"));
+        System.out.println(extractClaim(jwtToken,Claims::getSubject));
     }
 }
---
+++ src/main/java/org/example/uberprojectauthservice/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package org.example.uberprojectauthservice.services;
+
+import org.example.uberprojectauthservice.helpers.AuthPassengerDetails;
+import org.example.uberprojectauthservice.model.Passenger;
+import org.example.uberprojectauthservice.repository.PassengerRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final PassengerRepository passengerRepository;
+
+    public UserDetailsServiceImpl(PassengerRepository passengerRepository) {
+        this.passengerRepository = passengerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Passenger> passenger = passengerRepository.findPassengerByEmail(email);
+        if(passenger.isPresent()) {
+            return new AuthPassengerDetails(passenger.get());
+        }else {
+            throw new UsernameNotFoundException("cannot find the passenger by given email");
+        }
+    }
+}
